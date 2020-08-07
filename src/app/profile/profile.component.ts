7f9779aa14c8ce00ddc8cd3f78c73622b2d44d00
SANKET UTTARWAR <sanketuttarwar@gmail.com>
2020-08-07 18:59:08 +0530

    7/8/20/3

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -20,7 +20,6 @@ export class ProfileComponent implements OnInit {
     this.route.data.subscribe((data) => {
       this.user = data['user'];
     });
-    //this.loadUserDetails();
   }
 
   loadUserDetails() {
