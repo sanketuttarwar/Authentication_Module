e6685dc96fac33dd690f62971d996fa6a5c370b5
shrikar99 <52737970+shrikar99@users.noreply.github.com>
2020-08-10 14:37:58 +0530

    Updated with admin dashboard

---
+++ src/app/_models/data.model.ts
@@ -0,0 +1,15 @@
+export interface RequestDataStore{
+  assignedEmpId: number;
+  createdEmpId: number;
+  createdOn: Date;
+  lastModifiedBy: string;
+  lastModifiedOn: Date;
+  requestCategory: string;
+  requestDepartment: string;
+  requestId: string;
+  requestStatus: string;
+  requestSubCategory: string;
+  requestSummary: string;
+  requestType: string;
+  title: string;
+}
---
+++ src/app/_services/request-list.service.ts
@@ -0,0 +1,68 @@
+import {Injectable} from '@angular/core';
+import {RequestDataStore} from '../_models/data.model';
+import { Subject, Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RequestListService{
+  filterDisabled = false;
+  filteredRequests: RequestDataStore[];
+  isfilterApplied = false;
+  requestsChanged = new Subject<RequestDataStore []>();
+  requests: RequestDataStore [];
+  requestList: RequestDataStore [];
+  rootURL = 'https://localhost:44355/api/request';
+
+   constructor(private http: HttpClient){}
+
+   getRequests(): Observable<RequestDataStore[]>{
+    return this.http.get<RequestDataStore[]>(this.rootURL+'/getallrequests').pipe(
+      map((response) => {
+        const req: RequestDataStore[] = [];
+        for(const key in response){
+            req.push({...response[key]});
+        }
+        return req;
+      })
+    );
+   }
+
+   getRequest(id: string){
+     return this.requests.find(req => req.requestId === id);
+   }
+
+   filterValues( filterDepartment: string,
+    filterCategory: string,
+    filterSubCategory: string,
+    filterStatus: string,
+    filterType: string){
+            this.filteredRequests = this.requests;
+              if(filterDepartment !== ''){
+                this.filteredRequests = this.filteredRequests.filter(req => req.requestDepartment === filterDepartment);
+              }else{
+                this.filteredRequests = this.requests;
+              }
+              if(filterCategory !== ''){
+                this.filteredRequests = this.filteredRequests.filter(req => req.requestCategory === filterCategory);
+              }
+              if(filterSubCategory !== ''){
+                this.filteredRequests = this.filteredRequests.filter(req => req.requestSubCategory === filterSubCategory);
+              }
+              if(filterStatus !== ''){
+                this.filteredRequests = this.filteredRequests.filter(req => req.requestStatus === filterStatus);
+              }
+              if(filterType !== ''){
+                this.filteredRequests = this.filteredRequests.filter(req => req.requestType === filterType);
+              }
+          console.log(this.filteredRequests);
+
+    }
+    setFilteredValues(){
+      return this.filteredRequests;
+    }
+}
